840953ee9a6928ffb1eae5e797f5be15640ac931
xaedes <xaedes@gmail.com>
2020-01-05 21:31:50 +0100

    added particle life, controls:
    space: spawn particles
    c: clear particles
    r: randomize particles
    t: randomize types
    
    rightmouse+wasd: camera control

--- Assets/Scripts/ParticleLife.cs
+++ Assets/Scripts/ParticleLife.cs
@@ -12,28 +12,162 @@ using Unity.Burst;
 
 public class ParticleLife : MonoBehaviour
 {
-    public float friction = 0.9f;
-    public float strength = 10.0f;
-    public float radius = 5.0f;
+    [Header("Particle Types")]
+    //public int maxInteractions = 1000000;
+    public int numParticleTypes = 10;
+    public float attractMean = 0;
+    public float attractStd = 0.2f;
+    public float rangeMinLower = 0.0f;
+    public float rangeMinUpper = 20.0f;
+    public float rangeMaxLower = 10.0f;
+    public float rangeMaxUpper = 50.0f;
 
+    [Header("Simulation Properties")]
     public float minSimulationStepRate = 60.0f;
+    public float maxSpeed = 10.0f;
+    public float friction = 0.9f;
+    public float interactionStrength = 10.0f;
+    public float radius = 5.0f;
+    public float r_smooth = 2.0f;
+    public bool flatForce = false;
 
+    [Header("World Properties")]
     public float3 lowerBound = -100;
     public float3 upperBound = +100;
     public bool wrapX = true;
     public bool wrapY = true;
     public bool wrapZ = true;
     public float bounce = 1.0f;
-    public int spawnCount = 100;
 
+    [Header("Particle Instantiation")]
+    public int spawnCount = 100;
+    public float3 creationBoxSize = 0.5f;
+    public float3 creationBoxCenter = 0.5f;
+
+    [Header("Particle Visuals")]
     public Material particleMaterial;
     public Mesh particleMesh;
+
+
+
+    private int m_numTypes = 0;
+    private NativeArray<Color> m_Colors;
+    private NativeArray<float> m_Attract;
+    private NativeArray<float> m_RangeMin;
+    private NativeArray<float> m_RangeMax;
+    private List<Material> m_adjustedMaterials = new List<Material>();
+
 
     private static EntityManager m_manager;
     private EntityArchetype m_archetype;
     private ParticleLifeSystem m_particleLifeSystem;
-    public NativeList<Entity> entities;
-    // Start is called before the first frame update
+    private NativeList<Entity> m_entities;
+
+    void initParticleTypeArrays(int num)
+    {
+        if(m_Colors.IsCreated && m_Colors.Length>0) m_Colors.Dispose();
+        if (m_Attract.IsCreated && m_Attract.Length > 0) m_Attract.Dispose();
+        if (m_RangeMin.IsCreated && m_RangeMin.Length > 0) m_RangeMin.Dispose();
+        if (m_RangeMax.IsCreated && m_RangeMax.Length > 0) m_RangeMax.Dispose();
+        m_numTypes = num;
+        m_Colors = new NativeArray<Color>(m_numTypes, Allocator.Persistent);
+        m_Attract = new NativeArray<float>(m_numTypes * m_numTypes, Allocator.Persistent);
+        m_RangeMin = new NativeArray<float>(m_numTypes * m_numTypes, Allocator.Persistent);
+        m_RangeMax = new NativeArray<float>(m_numTypes * m_numTypes, Allocator.Persistent);
+        for (int i = 0; i < m_numTypes; i++)
+        {
+            m_Colors[i] = fromHSV((float)i / m_numTypes, 1.0f, i % 2 * 0.5f + 0.5f);
+        }
+        generateMaterials();
+    }
+    Color fromHSV(float h, float s, float v)
+    {
+        int i = (int)(h * 6);
+        float f = h * 6 - i;
+        float p = v * (1 - s);
+        float q = v * (1 - f * s);
+        float t = v * (1 - (1 - f) * s);
+
+        float r = 0;
+        float g = 0;
+        float b = 0;
+        switch (i % 6)
+        {
+            case 0: r = v; g = t; b = p; break;
+            case 1: r = q; g = v; b = p; break;
+            case 2: r = p; g = v; b = t; break;
+            case 3: r = p; g = q; b = v; break;
+            case 4: r = t; g = p; b = v; break;
+            case 5: r = v; g = p; b = q; break;
+        }
+
+        return new Color(r, g, b);
+    }
+
+    void generateMaterials()
+    {
+        m_adjustedMaterials.Clear();
+        for (int i = 0; i < m_numTypes; i++)
+        {
+            Color particle_color = m_Colors[i];
+            m_adjustedMaterials.Add(new Material(particleMaterial) { color = particle_color });
+        }
+    }
+    public static float nextGaussianFloat()
+    {
+        float u, v, S;
+
+        do
+        {
+            u = 2.0f * UnityEngine.Random.value - 1.0f;
+            v = 2.0f * UnityEngine.Random.value - 1.0f;
+            S = u * u + v * v;
+        }
+        //while ((S >= 1.0f));
+        while ((S >= 1.0f) || (math.abs(S) < 1e-6));
+
+        float fac = math.sqrt(-2.0f * math.log(S) / S);
+        return u * fac;
+    }
+    void setRandomTypes()
+    {
+        if (!m_Attract.IsCreated || !m_RangeMin.IsCreated || !m_RangeMax.IsCreated)
+        {
+            Debug.LogError("setRandomParticleTypes called but Attract, RangeMin or RangeMax are nor created.");
+            return;
+        }
+        int num2 = m_numTypes * m_numTypes;
+        if ((m_Attract.Length != num2) || (m_RangeMin.Length != num2) || (m_RangeMax.Length != num2))
+        {
+            Debug.LogError("setRandomParticleTypes called but Attract, RangeMin or RangeMax have wrong size.");
+            return;
+        }
+        for (int i = 0; i < m_numTypes; i++)
+        {
+            for (int k = 0; k < m_numTypes; k++)
+            {
+                int coord = i * m_numTypes + k;
+                int icoord = k * m_numTypes + i;
+                if (i == k)
+                {
+                    // coord == icoord
+                    m_Attract[coord] = -math.abs(nextGaussianFloat() * attractStd + attractMean);
+                    //m_Attract[coord] = (nextGaussianFloat(r) * attractStd + attractMean);
+                    m_RangeMin[coord] = radius * 2;
+                }
+                else
+                {
+                    m_Attract[coord] = nextGaussianFloat() * attractStd + attractMean;
+                    m_Attract[icoord] = nextGaussianFloat() * attractStd + attractMean;
+                    m_RangeMin[coord] = math.max(radius * 2, UnityEngine.Random.Range(rangeMinLower, rangeMinUpper));
+                }
+                m_RangeMax[coord] = math.max(m_RangeMin[coord], UnityEngine.Random.Range(rangeMaxLower, rangeMaxUpper));
+                m_RangeMin[icoord] = m_RangeMin[coord];
+                m_RangeMax[icoord] = m_RangeMax[coord];
+            }
+        }
+    }
+
     void Start()
     {
         m_manager = World.DefaultGameObjectInjectionWorld.EntityManager;
@@ -41,49 +175,123 @@ public class ParticleLife : MonoBehaviour
             typeof(Translation),
             typeof(LocalToWorld),
             typeof(RenderMesh),
-            typeof(Particle)
+            typeof(Particle),
+            typeof(Scale)
         );
         m_particleLifeSystem = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<ParticleLifeSystem>();
         m_particleLifeSystem.particleLife = this;
+        initParticleTypeArrays(numParticleTypes);
+        setRandomTypes();
+        setSystemTypeArrays();
+        addParticles(spawnCount);
     }
 
     private void OnEnable()
     {
-        entities = new NativeList<Entity>(10000, Allocator.Persistent);
+        m_entities = new NativeList<Entity>(10000, Allocator.Persistent);
     }
+
     private void OnDisable()
     {
-        entities.Dispose();
+        m_entities.Dispose();
+        m_Colors.Dispose();
+        m_Attract.Dispose();
+        m_RangeMin.Dispose();
+        m_RangeMax.Dispose();
+    }
+
+    void setSystemTypeArrays()
+    {
+        m_particleLifeSystem.Attract = m_Attract;
+        m_particleLifeSystem.RangeMin = m_RangeMin;
+        m_particleLifeSystem.RangeMax = m_RangeMax;
+        m_particleLifeSystem.numTypes = m_numTypes;
+    }
+    
+    void updateParticleTypesAndMaterials()
+    {
+        if (m_adjustedMaterials.Count == 0) return;
+        for (int i = 0; i < m_entities.Length; i++)
+        {
+            var particle = m_manager.GetComponentData<Particle>(m_entities[i]);
+            particle.type = particle.type % m_adjustedMaterials.Count;
+            
+            var mat = (particle.type < m_adjustedMaterials.Count) 
+                ? m_adjustedMaterials[particle.type] 
+                : particleMaterial;
+            m_manager.SetComponentData<Particle>(m_entities[i], particle);
+            m_manager.SetSharedComponentData(m_entities[i], 
+                new RenderMesh { material = mat, mesh = particleMesh });
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (numParticleTypes != m_numTypes)
+        {
+            initParticleTypeArrays(numParticleTypes);
+            setRandomTypes();
+            setSystemTypeArrays();
+            updateParticleTypesAndMaterials();
+        }
         if (Input.GetKeyDown("space"))
         {
-            AddParticles(spawnCount);
+            addParticles(spawnCount);
         }
+        if (Input.GetKeyDown("c"))
+        {
+            clearParticles();
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            var num = m_entities.Length;
+            clearParticles();
+            addParticles(num);
+        }
+        if (Input.GetKeyDown("t"))
+        {
+            setRandomTypes();
+            setSystemTypeArrays();
+        }
+    }
+    void clearParticles()
+    {
+        for (int i = 0; i < m_entities.Length; i++)
+        {
+            m_manager.DestroyEntity(m_entities[i]);
+        }
+        m_entities.Clear();
+        //fps.numParticles = particles.Count;
     }
 
 
-    void AddParticles(int num)
+    void addParticles(int num)
     {
         NativeArray<Entity> particles = new NativeArray<Entity>(num, Allocator.Temp);
 
         m_manager.CreateEntity(m_archetype, particles);
 
+        float3 dim = upperBound - lowerBound;
+        float3 boxCenter = creationBoxCenter * dim + lowerBound;
+        float3 posLowerBound = boxCenter - 0.5f * creationBoxSize * dim;
+        float3 posUpperBound = boxCenter + 0.5f * creationBoxSize * dim;
+
         for (int i = 0; i < num; i++)
         {
-            float x = Random.Range(lowerBound.x, upperBound.x);
-            float y = Random.Range(lowerBound.y, upperBound.y);
-            float z = Random.Range(lowerBound.z, upperBound.z);
+            float x = Random.Range(posLowerBound.x, posUpperBound.x);
+            float y = Random.Range(posLowerBound.y, posUpperBound.y);
+            float z = Random.Range(posLowerBound.z, posUpperBound.z);
             float vx = Random.Range(-2.0f, 2.0f);
             float vy = Random.Range(-2.0f, 2.0f);
             float vz = Random.Range(-2.0f, 2.0f);
+            int type = Random.Range(0, m_numTypes);
+            var mat = (type < m_adjustedMaterials.Count) ? m_adjustedMaterials[type] : particleMaterial;
             m_manager.SetComponentData(particles[i], new Translation { Value = new float3(x, y, z) });
-            m_manager.SetComponentData(particles[i], new Particle { type = 0, vel = new float3(vx, vy, vz), cell_number=-1 });
-            m_manager.SetSharedComponentData(particles[i], new RenderMesh { material = particleMaterial, mesh = particleMesh });
-            entities.Add(particles[i]);
+            m_manager.SetComponentData(particles[i], new Particle { type = type, vel = new float3(vx, vy, vz), cell_number=-1 });
+            m_manager.SetComponentData(particles[i], new Scale { Value = radius*2f });
+            m_manager.SetSharedComponentData(particles[i], new RenderMesh { material = mat, mesh = particleMesh });
+            m_entities.Add(particles[i]);
         }
 
         particles.Dispose();
--- Assets/Scripts/ParticleLifeSystem.cs
+++ Assets/Scripts/ParticleLifeSystem.cs
@@ -114,6 +114,7 @@ public class ParticleLifeSystem : JobComponentSystem
 
         public float deltaTime;
         public float friction;
+        public float maxSpeed;
 
         public float3 lowerBound;
         public float3 upperBound;
@@ -126,6 +127,12 @@ public class ParticleLifeSystem : JobComponentSystem
         public float eps;
         public float strength;
         public float radius;
+        public float r_smooth;
+        public bool flatForce;
+        public int numTypes;
+        [ReadOnly] public NativeArray<float> Attract;
+        [ReadOnly] public NativeArray<float> RangeMin;
+        [ReadOnly] public NativeArray<float> RangeMax;
 
         //public ComponentDataFromEntity<Translation> translations;
         //public ComponentDataFromEntity<Particle> particles;
@@ -137,6 +144,7 @@ public class ParticleLifeSystem : JobComponentSystem
         //[DeallocateOnJobCompletion] public NativeArray<ArchetypeChunk> Chunks;
         //public ArchetypeChunkComponentType<Translation> TranslationType;
         //public ArchetypeChunkComponentType<Particle> ParticleType;
+        private int C(int x, int y) => y * numTypes + x;
 
         public void Execute(int index)
         {
@@ -148,37 +156,78 @@ public class ParticleLifeSystem : JobComponentSystem
             Translation translation = oldTranslations[index];
             Particle particle = oldParticles[index];
 
-            int num = particles.Length;
-            for (int k = 0; k < num; k++)
-            {
-                Translation otherTranslation = oldTranslations[k];
-                Particle otherParticle = oldParticles[k];
-                float3 diff = otherTranslation.Value - translation.Value;
-                if (wrapX && dim.x > 0)
+            if (numTypes > 0)
+            { 
+                int num = particles.Length;
+                for (int k = 0; k < num; k++)
                 {
-                    while (diff.x < -halfDim.x) diff.x += dim.x;
-                    while (diff.x > +halfDim.x) diff.x -= dim.x;
+                    Translation otherTranslation = oldTranslations[k];
+                    Particle otherParticle = oldParticles[k];
+                    float3 diff = otherTranslation.Value - translation.Value;
+                    if (wrapX && dim.x > 0)
+                    {
+                        while (diff.x < -halfDim.x) diff.x += dim.x;
+                        while (diff.x > +halfDim.x) diff.x -= dim.x;
+                    }
+                    if (wrapY && dim.y > 0)
+                    {
+                        while (diff.y < -halfDim.y) diff.y += dim.y;
+                        while (diff.y > +halfDim.y) diff.y -= dim.y;
+                    }
+                    if (wrapZ && dim.z > 0)
+                    {
+                        while (diff.z < -halfDim.z) diff.z += dim.z;
+                        while (diff.z > +halfDim.z) diff.z -= dim.z;
+                    }
+                    float r2 = diff.x * diff.x + diff.y * diff.y + diff.z * diff.z;
+                    var typeA = particle.type % numTypes;
+                    var typeB = otherParticle.type % numTypes;
+                    var coord = C(typeA, typeB);
+                    float r_max = RangeMax[coord];
+                    if (r2 > r_max * r_max) continue;
+                    float r = math.sqrt(r2);
+                    if (r < eps) continue;
+                    float3 direction = diff / r;
+                    var r_min = RangeMin[coord];
+                    float f;
+                    if (r > r_min)
+                    {
+                        var attract = Attract[coord];
+                        if (flatForce)
+                        {
+                            f = attract;
+                        }
+                        else
+                        {
+                            var rangeMid = (r_min + r_max) * 0.5f;
+                            var numer = 2.0f * math.abs(r - rangeMid);
+                            var denom = r_max - r_min;
+                            if (denom > eps)
+                            {
+                                f = attract * (1.0f - numer / denom);
+                            }
+                            else
+                            {
+                                f = attract;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if ((r + r_smooth > eps) && (r_min + r_smooth > eps))
+                        {
+                            f = r_smooth * r_min * (1.0f / (r_min + r_smooth) - 1.0f / (r + r_smooth));
+                        }
+                        else
+                        {
+                            f = 0f;
+                        }
+                    }
+
+                    particle.vel = particle.vel + direction * f * strength * deltaTime;
+                    //otherParticle.vel = otherParticle.vel - direction * deltaTime * f;
+                    //particles[k] = otherParticle;
                 }
-                if (wrapY && dim.y > 0)
-                {
-                    while (diff.y < -halfDim.y) diff.y += dim.y;
-                    while (diff.y > +halfDim.y) diff.y -= dim.y;
-                }
-                if (wrapZ && dim.z > 0)
-                {
-                    while (diff.z < -halfDim.z) diff.z += dim.z;
-                    while (diff.z > +halfDim.z) diff.z -= dim.z;
-                }
-                float r2 = diff.x * diff.x + diff.y * diff.y + diff.z * diff.z;
-                float r_max = radius * 10;
-                if (r2 > r_max * r_max) continue;
-                float r = math.sqrt(r2);
-                if (r < eps) continue;
-                float3 direction = diff / r;
-                float f = -strength;
-                particle.vel = particle.vel + direction * deltaTime * f;
-                //otherParticle.vel = otherParticle.vel - direction * deltaTime * f;
-                //particles[k] = otherParticle;
             }
 
             #region movement
@@ -250,12 +299,73 @@ public class ParticleLifeSystem : JobComponentSystem
             }
             #endregion
             #endregion
+
+            #region max speed
+            float speed = math.sqrt(particle.vel.x * particle.vel.x + particle.vel.y * particle.vel.y + particle.vel.z * particle.vel.z);
+            if (speed > eps)
+                if (speed > maxSpeed)
+                {
+                    particle.vel.x *= maxSpeed / speed;
+                    particle.vel.y *= maxSpeed / speed;
+                    particle.vel.z *= maxSpeed / speed;
+                }
+            #endregion
             translations[index] = translation;
             particles[index] = particle;
         }
     }
 
     EntityQuery m_query;
+
+    public int numTypes
+    {
+        get { return m_newNumTypes; }
+        set
+        {
+            m_newNumTypes = value;
+            m_NumTypesChanged = true;
+        }
+    }
+    public NativeArray<float> Attract
+    {
+        get { return m_newAttract; }
+        set
+        {
+            m_newAttract = value;
+            m_AttractChanged = true;
+        }
+    }
+    public NativeArray<float> RangeMin
+    {
+        get { return m_newRangeMin; }
+        set
+        {
+            m_newRangeMin = value;
+            m_RangeMinChanged = true;
+        }
+    }
+    public NativeArray<float> RangeMax
+    {
+        get { return m_newRangeMax; }
+        set
+        {
+            m_newRangeMax = value;
+            m_RangeMaxChanged = true;
+        }
+    }
+
+    bool m_NumTypesChanged = false;
+    bool m_AttractChanged = false;
+    bool m_RangeMinChanged = false;
+    bool m_RangeMaxChanged = false;
+    int m_newNumTypes;
+    NativeArray<float> m_newAttract;
+    NativeArray<float> m_newRangeMin;
+    NativeArray<float> m_newRangeMax;
+    int m_copyOfNumTypes = 0;
+    NativeArray<float> m_copyOfAttract = new NativeArray<float>(0, Allocator.Persistent);
+    NativeArray<float> m_copyOfRangeMin = new NativeArray<float>(0, Allocator.Persistent);
+    NativeArray<float> m_copyOfRangeMax = new NativeArray<float>(0, Allocator.Persistent);
 
     protected override void OnCreate()
     {
@@ -265,10 +375,48 @@ public class ParticleLifeSystem : JobComponentSystem
         };
         m_query = GetEntityQuery(queryDesc);
     }
-
+    protected override void OnDestroy()
+    {
+        m_copyOfAttract.Dispose();
+        m_copyOfRangeMin.Dispose();
+        m_copyOfRangeMax.Dispose();
+    }
     protected override JobHandle OnUpdate(JobHandle inputDependencies)
     {
+        // assign new particle type info in sync with the jobs
+        if (m_NumTypesChanged)
+        {
+            m_copyOfNumTypes = numTypes;
+            m_NumTypesChanged = false;
+        }
+        if (m_AttractChanged && Attract.IsCreated)
+        {
+            if (m_copyOfAttract.IsCreated || m_copyOfAttract.Length > 0) m_copyOfAttract.Dispose();
+            m_copyOfAttract = new NativeArray<float>(Attract, Allocator.Persistent);
+            m_AttractChanged = false;
+        }
+        if (m_RangeMinChanged && RangeMin.IsCreated)
+        {
+            if (m_copyOfRangeMin.IsCreated || m_copyOfRangeMin.Length > 0) m_copyOfRangeMin.Dispose();
+            m_copyOfRangeMin = new NativeArray<float>(RangeMin, Allocator.Persistent);
+            m_RangeMinChanged = false;
+        }
+        if (m_RangeMaxChanged && RangeMax.IsCreated)
+        {
+            if (m_copyOfRangeMax.IsCreated || m_copyOfRangeMax.Length > 0) m_copyOfRangeMax.Dispose();
+            m_copyOfRangeMax = new NativeArray<float>(RangeMax, Allocator.Persistent);
+            m_RangeMaxChanged = false;
+        }
+
+        //// only do jobs when particle type info is existing and consistent
+        //if ((!m_copyOfAttract.IsCreated || !m_copyOfRangeMin.IsCreated || !m_copyOfRangeMax.IsCreated)
+        //    || (m_copyOfAttract.Length != m_copyOfNumTypes * m_copyOfNumTypes)
+        //    || (m_copyOfRangeMin.Length != m_copyOfNumTypes * m_copyOfNumTypes)
+        //    || (m_copyOfRangeMax.Length != m_copyOfNumTypes * m_copyOfNumTypes))
+        //    return inputDependencies;
+
         var job = new ParticleLifeSystemJobFor();
+
 
         //var TranslationType = GetArchetypeChunkComponentType<Translation>();
         //var ParticleType = GetArchetypeChunkComponentType<Particle>(true);
@@ -287,7 +435,18 @@ public class ParticleLifeSystem : JobComponentSystem
         {
             job.deltaTime = UnityEngine.Time.deltaTime;
         }
+        job.radius = particleLife.radius;
+        job.r_smooth = particleLife.r_smooth;
+        job.flatForce = particleLife.flatForce;
         job.friction = particleLife.friction;
+        job.strength = particleLife.interactionStrength;
+        job.maxSpeed = particleLife.maxSpeed;
+
+        job.numTypes = m_copyOfNumTypes;
+        job.Attract = m_copyOfAttract;
+        job.RangeMin = m_copyOfRangeMin;
+        job.RangeMax = m_copyOfRangeMax;
+
         job.lowerBound = particleLife.lowerBound;
         job.upperBound = particleLife.upperBound;
         job.dim = job.upperBound - job.lowerBound;
@@ -296,8 +455,7 @@ public class ParticleLifeSystem : JobComponentSystem
         job.wrapY = particleLife.wrapY;
         job.wrapZ = particleLife.wrapZ;
         job.bounce = particleLife.bounce;
-        job.strength = particleLife.strength;
-        job.radius = particleLife.radius;
+
         job.oldTranslations = m_query.ToComponentDataArray<Translation>(Allocator.TempJob);
         job.oldParticles = m_query.ToComponentDataArray<Particle>(Allocator.TempJob);
         job.translations = m_query.ToComponentDataArray<Translation>(Allocator.TempJob);
@@ -306,7 +464,7 @@ public class ParticleLifeSystem : JobComponentSystem
         //job.particles = GetComponentDataFromEntity<Particle>(false);
 
         // Now that the job is set up, schedule it to be run. 
-        var jobHandle = job.Schedule(particleLife.entities.Length, 16, inputDependencies);
+        var jobHandle = job.Schedule(job.particles.Length, 16, inputDependencies);
         jobHandle.Complete();
         m_query.CopyFromComponentDataArray<Translation>(job.translations);
         m_query.CopyFromComponentDataArray<Particle>(job.particles);
